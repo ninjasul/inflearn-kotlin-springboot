c95a15db0e165d342d3d40f686673669201278e0
박동엽 <dypark@onestorecorp.com>
2022-08-22 12:17:29 +0900

    findByIdOrThrow 오류 수정

--- src/main/kotlin/com/group/libraryapp/util/ExceptionUtils.kt
+++ src/main/kotlin/com/group/libraryapp/util/ExceptionUtils.kt
@@ -1,11 +1,12 @@
 package com.group.libraryapp.util
 
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.findByIdOrNull
 
 fun fail(): Nothing {
     throw IllegalArgumentException()
 }
 
 fun <T, ID> CrudRepository<T, ID>.findByIdOrThrow(id: ID): T {
-    return this.findByIdOrThrow(id) ?: fail()
+    return this.findByIdOrNull(id) ?: fail()
 }
